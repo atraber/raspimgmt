9f8ed90454eedf59a63811b939e2c0b4e09aaf46
Andreas Traber <andytraber@gmail.com>
2018-05-13 12:53:34 +0200

    Add screen enable boolean for devices

--- src/app/device.ts
+++ src/app/device.ts
@@ -4,12 +4,14 @@ export class Device {
   id: number;
   name: string;
   mac: string;
+  screen_enable: Boolean;
   last_seen: number;
   streams: Stream[];
 
   constructor () {
     this.name = "";
     this.mac = "";
+    this.screen_enable = true;
     this.last_seen = 0;
     this.streams = [];
   }
